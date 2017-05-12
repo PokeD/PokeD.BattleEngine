0cace801290d641a64857ddf0b28edb3677ef497
Aragas <micvitalij@yandex.ru>
2017-05-12 16:11:11 +0300

    Removed User in BaseAttackInstance

--- Attack/BaseAttackInstance.cs
+++ Attack/BaseAttackInstance.cs
@@ -8,15 +8,13 @@ namespace PokeD.BattleEngine.Attack
     {
         public IAttackStaticData StaticData { get; }
 
-        public IMonsterStaticData User { get; }
-
         public byte PPCurrent { get; }
         public byte PPUps { get; }
         public virtual byte PPMax => (byte) (StaticData.PP + (byte) Math.Round((double) StaticData.PP * PPUps * 0.20D));
 
 
-        public BaseAttackInstance(IAttackStaticData staticData, IMonsterStaticData user) { StaticData = staticData; User = user; PPCurrent = StaticData.PP; }
-        public BaseAttackInstance(IAttackStaticData staticData, byte pp, byte ppUps, IMonsterStaticData user) : this(staticData, user) { PPCurrent = pp; PPUps = ppUps; }
+        public BaseAttackInstance(IAttackStaticData staticData) { StaticData = staticData; PPCurrent = StaticData.PP; }
+        public BaseAttackInstance(IAttackStaticData staticData, byte pp, byte ppUps) : this(staticData) { PPCurrent = pp; PPUps = ppUps; }
 
         public override string ToString() => $"{PPCurrent,2}/{PPMax,2} {StaticData}";
     }
